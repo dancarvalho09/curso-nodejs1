ba1da2b810ce7887d14a7228937be91c9f92f91d
Danúbia Carvalho <74597179+dancarvalho09@users.noreply.github.com>
2022-11-08 19:16:07 -0300

    Update index.js

--- Teste/index.js
+++ Teste/index.js
@@ -4,7 +4,7 @@ const express = require("express"); //require("módulo") - retorna uma função
 const app = express(); //Qualquer coisa relacionado ao express será usada a variável app
 
 app.get("/", function(req, res) {
-   res.send("Bem vindo ao meu app!"); //send = aparece a mensagem no navegador
+   res.send("Bem vindo ao meu app!"); //send = aparece a mensagem no navegador. É possível apenas um único send por rota
 })
 
 app.get("/sobre", function(req, res) {
@@ -13,6 +13,10 @@ app.get("/sobre", function(req, res) {
 
 app.get("/blog", function(req, res) {
     res.send("Bem vindo ao meu blog");
+})
+
+app.get("/ola/:nome/:cargo", function(req, res) {    // o que vier depois dos ":" é o parâmetro
+    res.send("<h1> Ola " + req.params.nome + "</h1>" + "<h2> Seu cargo: " + req.params.cargo + "</h2>"); // res.send(req.params) - simples
 })
 
 
